6ee4e6f27eeb95ff4c155aa051ecce63dc733fe4
Alyssa Mannie <alyssamannie13@gmail.com>
2024-11-13 23:13:10 -0700

    Just Updated

--- rsh.c
+++ rsh.c
@@ -15,8 +15,17 @@ int isAllowed(const char*cmd) {
 	// TODO
 	// return 1 if cmd is one of the allowed commands
 	// return 0 otherwise
-	
+	for (int i = 0; i < N; i++) {
+		if (strcmp(cmd, allowed[i]) == 0) {
+			return i + 1;
+		}
+	}
+
 	return 0;
+}
+
+void print_help() {
+	
 }
 
 int main() {
@@ -25,6 +34,7 @@ int main() {
     // Add variables as needed
 
     char line[256];
+    char *argv[21];
 
     while (1) {
 
@@ -40,7 +50,49 @@ int main() {
 	// Add code to spawn processes for the first 9 commands
 	// And add code to execute cd, exit, help commands
 	// Use the example provided in myspawn.c
+	//
+	int argc = 0;
+	char *token = strtok(line, " ");
+	while (token != NULL && argc < 20) {
+		argv[argc++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[argc] = NULL;
+
+	int cmd_index = isAllowed(argv[0]) - 1;
+
+	if (cmd_index == -1){
+		printf("NOT ALLOWED!\n");
+		continue;
+    }
+
+	if (cmd_index == 9) {
+		if (argc > 2) {
+			printf("-rsh: cd: too many arguments\n");
+		} else if (argc == 1 || chdir(argv[1]) !=0) {
+			perror("-rsh: cd");
+		}
+	} else if (cmd_index == 10) {
+		break;
+	} else if (cmd_index == 11) {
+		printf("The allowed commands are:\n");
+		for (int i = 0; i < N; i++) {
+			printf("5d: 5s\n", i + 1, allowed[i];
+	} else {
+		pid_t pid;
+		int status;
+		if (posix_spawnp(&pid, argv[0], NULL, NULL, argv, environ) != 0) {
+			perror("-rsh: posix_spawnp failed");
+		} else {
+			if (waitpid(pid, &status, 0) == -1) {
+				perror("-rsh: waitpid failed");
+			} else if (WIFEXITED(status)) {
+				printf("Process exited with status %d\n", WEXITSATUS(status));
+			}
+		}
+	}
 
     }
     return 0;
 }
+
